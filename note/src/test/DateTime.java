6978130f7c5aa479b13e6553a3238eb82922bd6e
jiansp <kkjane@163.com>
2017-07-28 15:04:12 +0800

    date

--- note/src/test/DateTime.java
+++ note/src/test/DateTime.java
@@ -1,5 +1,19 @@
 package test;
 
-public class DateTime {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.junit.Test;
+
+public class DateTime {
+	@Test
+	public void main(){
+		/*
+		 * 1.格式化时间成String类型
+		 */ 
+		SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		String date = formatDate.format(new Date());
+		System.out.println(date);
+		
+	}
 }
