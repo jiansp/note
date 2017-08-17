5dbb66542bee26ba5dbf9406f5b8d6b3489dff8d
jiansp <kkjane@163.com>
2017-08-17 11:36:36 +0800

    2017.08.17

--- note/src/com/jian/dao/TestDao.java
+++ note/src/com/jian/dao/TestDao.java
@@ -1,12 +1,53 @@
 package com.jian.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.ejb.FinderException;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
+import org.junit.Test;
 
 import com.jian.moble.ParamConfig;
 
 public class TestDao {
+	
+	@Test
+	public void test() {
+		String ql = "select p.id,p.typeName from ParamConfig p where p.id='555af11ce83345b397f14a2bb80193e5'";
+		Session session = HibernateUtil.getSession();
+		session.beginTransaction();
+		Object[] ret = findArrayByHql(ql);
+		for(Object str : ret){
+			System.out.println(str);
+		}
+		
+	}
+	/*
+	 * 创建Query对象时
+	 * 1、sql使用session.createSQLQuery方法
+	 * 2、hql使用session.createQuery方法
+	 */
+	public List findBySql(String sql){
+		Session session = HibernateUtil.getSession();
+		Query query = session.createSQLQuery(sql);
+		return query.list();
+	}
+	
+	public List findByHql(String hql){
+		Session session = HibernateUtil.getSession();
+		Query query = session.createQuery(hql);
+		return query.list();
+	}
+	/*
+	 * 当hql中选择查询字段时，返回的不是实例的集合，而是数组
+	 */
+	public Object[] findArrayByHql(String hql){
+		Session session = HibernateUtil.getSession();
+		Query query = session.createQuery(hql);
+		return (Object[])query.uniqueResult();
+	}
 	
 	public void save(ParamConfig paranConfig){
 		Session session = HibernateUtil.getSession();
